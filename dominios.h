7f2552c0556b39b868c23bfb942c27312284fbe5
Nicole <nfgomes2509@gmail.com>
2024-04-04 09:01:32 -0300

    classe Dominio

--- dominios.h
+++ dominios.h
@@ -5,5 +5,18 @@
 #include <stdexcept>
 using namespace std;
 
+class Dominio {
+private:
+    virtual void validar(string) = 0;
+protected:
+    string valor;
+public:
+    virtual void setValor(string) = 0;
+    string getValor() const;
+};
+
+inline string Dominio::getValor() const {
+    return valor;
+};
 
 #endif // DOMINIOS_HPP_INCLUDED
