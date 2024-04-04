45a4c3867f634a9bd533da37529f6167672cde85
Nicole <nfgomes2509@gmail.com>
2024-04-04 09:58:19 -0300

    validação CodigoPagamento

--- dominios.cpp
+++ dominios.cpp
@@ -2,3 +2,21 @@
 
 #include <iostream>
 using namespace std;
+
+void CodigoPagamento::validar(string valor) {
+    if (valor.size() != 8) {
+        throw invalid_argument("Código de pagamento não possui 8 números.");
+    };
+
+    if (valor[0] == 0) {
+        throw invalid_argument("Primeiro digito inválido.");
+    };
+
+    for (int i = 0; i < 8; i++) {
+        char caractere = valor[i];
+
+        if (!(caractere >= '0' & caractere <= '9')) {
+            throw invalid_argument("Senha possui caractere inválido.");
+        };
+    };
+};
