ac57c6c1fd5193a858efa9d67cf3b47e927bb6dd
Nicole <nfgomes2509@gmail.com>
2024-04-04 08:56:29 -0300

    first commit

---
+++ dominios.cpp
@@ -0,0 +1,4 @@
+#include "dominios.h"
+
+#include <iostream>
+using namespace std;
---
+++ dominios.h
@@ -0,0 +1,9 @@
+#ifndef DOMINIOS_H_INCLUDED
+#define DOMINIOS_H_INCLUDED
+
+#include <string>
+#include <stdexcept>
+using namespace std;
+
+
+#endif // DOMINIOS_HPP_INCLUDED
