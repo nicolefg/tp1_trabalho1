ac560f852b44ae6bff0cb615ea659f2eccc6458d
Nicole <nfgomes2509@gmail.com>
2024-04-16 14:11:01 -0300

    Classe Titulo

--- entidades.h
+++ entidades.h
@@ -43,5 +43,77 @@ inline Senha Conta::getSenha() const {
     return senha;
 };
 
+class Titulo {
+    private:
+        CodigoTitulo codigo;
+        Nome emissor;
+        Setor setor;
+        Data emissao;
+        Data vencimento;
+        Dinheiro valor;
+
+    public:
+        void setCodigo(const CodigoTitulo&);
+        CodigoTitulo getCodigo() const;
+        void setEmissor(const Nome&);
+        Nome getEmissor() const;
+        void setSetor(const Setor&);
+        Setor getSetor() const;
+        void setEmissao(const Data&);
+        Data getEmissao() const;
+        void setVencimento(const Data&);
+        Data getVencimento() const;
+        void setValor(const Dinheiro&);
+        Dinheiro getValor() const;
+};
+
+inline void Titulo::setCodigo(const CodigoTitulo &codigo){
+    this->codigo = codigo;
+};
+
+inline CodigoTitulo Titulo::getCodigo() const {
+    return codigo;
+};
+
+inline void Titulo::setEmissor(const Nome &emissor){
+    this->emissor = emissor;
+};
+
+inline Nome Titulo::getEmissor() const {
+    return emissor;
+};
+
+inline void Titulo::setSetor(const Setor &setor){
+    this->setor = setor;
+};
+
+inline Setor Titulo::getSetor() const {
+    return setor;
+};
+
+inline void Titulo::setEmissao(const Data &emissao){
+    this->emissao = emissao;
+};
+
+inline Data Titulo::getEmissao() const {
+    return emissao;
+};
+
+inline void Titulo::setVencimento(const Data &vecimento){
+    this->vencimento = vencimento;
+};
+
+inline Data Titulo::getVencimento() const {
+    return vencimento;
+};
+
+inline void Titulo::setValor(const Dinheiro &valor){
+    this->valor = valor;
+};
+
+inline Dinheiro Titulo::getValor() const {
+    return valor;
+};
+
 
 #endif // ENTIDADES_HPP_INCLUDED
