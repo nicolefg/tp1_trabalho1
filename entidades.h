34941ee0e5e8d083cbe345d8baab5350d76ed0a1
Nicole <nfgomes2509@gmail.com>
2024-04-16 14:10:01 -0300

    Classe Conta

--- entidades.h
+++ entidades.h
@@ -3,4 +3,45 @@
 
 #include "dominios.h"
 
+class Conta {
+    private:
+        CPF cpf;
+        Nome nome;
+        Senha senha;
+    
+    public:
+        void setCPF(const CPF&);
+        CPF getCPF() const;
+        void setNome(const Nome&);
+        Nome getNome() const;
+        void setSenha(const Senha&);
+        Senha getSenha() const;
+
+};
+
+inline void Conta::setCPF(const CPF &cpf){
+    this->cpf = cpf;
+};
+
+inline CPF Conta::getCPF() const {
+    return cpf;
+};
+
+inline void Conta::setNome(const Nome &nome){
+    this->nome = nome;
+};
+
+inline Nome Conta::getNome() const {
+    return nome;
+};
+
+inline void Conta::setSenha(const Senha &senha){
+    this->senha = senha;
+};
+
+inline Senha Conta::getSenha() const {
+    return senha;
+};
+
+
 #endif // ENTIDADES_HPP_INCLUDED
