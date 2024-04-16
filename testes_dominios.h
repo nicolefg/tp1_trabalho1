576b0c9385909412b96afc26a8ca6dd62b4c499f
Nicole <nfgomes2509@gmail.com>
2024-04-16 14:26:30 -0300

    Classe TesteDominio

--- testes_dominios.h
+++ testes_dominios.h
@@ -8,6 +8,19 @@ using namespace std;
 #include "dominios.h"
 #include "entidades.h"
 
+class TesteDominio {
+    private:                          
+        virtual void setUp();    
+        virtual void tearDown();  
+        virtual void testarCenarioSucesso();
+        virtual void testarCenarioFalha(); 
+    protected:
+        const string SUCESSO =  "Sucesso!";
+        const string FALHA = "Erro.";
+        string estado;   
+    public:
+        int run();
+};
 
 
 #endif // TESTES_H_DOM_INCLUDED
