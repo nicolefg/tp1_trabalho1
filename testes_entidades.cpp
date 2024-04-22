df5902727fccd438cfe9476a33ec02602dc8f952
Nicole <nfgomes2509@gmail.com>
2024-04-22 18:50:51 -0300

    Testes das entidades

--- testes_entidades.cpp
+++ testes_entidades.cpp
@@ -1,1 +1,116 @@
 #include "testes_entidades.h"
+
+string TesteEntidade::run(){
+    setUp();
+    testarCenarioSucesso();
+    tearDown();
+    return situacao;
+};
+
+void TesteConta::setUp(){
+    conta = new Conta();
+    situacao = SUCESSO;
+}
+void TesteConta::tearDown(){
+    delete conta;
+}
+
+void TesteConta::testarCenarioSucesso(){
+    CPF cpf;
+    cpf.setValor(CPF_VALIDO);
+    conta->setCPF(cpf);
+    if(conta->getCPF().getValor() != CPF_VALIDO)
+        situacao = FALHA;
+
+    Nome nome;
+    nome.setValor(NOME_VALIDO);
+    conta->setNome(nome);
+    if(conta->getNome().getValor() != NOME_VALIDO)
+        situacao = FALHA;
+
+    Senha senha;
+    senha.setValor(SENHA_VALIDA);
+    conta->setSenha(senha);
+    if(conta->getSenha().getValor() != SENHA_VALIDA)
+        situacao = FALHA;
+};
+
+void TesteTitulo::setUp(){
+    titulo = new Titulo();
+    situacao = SUCESSO;
+}
+void TesteTitulo::tearDown(){
+    delete titulo;
+}
+
+void TesteTitulo::testarCenarioSucesso(){
+    CodigoTitulo codigo;
+    codigo.setValor(CODIGOTIT_VALIDO);
+    titulo->setCodigo(codigo);
+    if(titulo->getCodigo().getValor() != CODIGOTIT_VALIDO)
+        situacao = FALHA;
+
+    Nome emissor;
+    emissor.setValor(EMISSOR_VALIDO);
+    titulo->setEmissor(emissor);
+    if(titulo->getEmissor().getValor() != EMISSOR_VALIDO)
+        situacao = FALHA;
+
+    Setor setor;
+    setor.setValor(SETOR_VALIDO);
+    titulo->setSetor(setor);
+    if(titulo->getSetor().getValor() != SETOR_VALIDO)
+        situacao = FALHA;
+
+    Data emissao;
+    emissao.setValor(EMISSAO_VALIDA);
+    titulo->setEmissao(emissao);
+    if(titulo->getEmissao().getValor() != EMISSAO_VALIDA)
+        situacao = FALHA;
+
+    Data vencimento;
+    vencimento.setValor(VENCIMENTO_VALIDO);
+    titulo->setVencimento(vencimento);
+    if(titulo->getVencimento().getValor() != VENCIMENTO_VALIDO)
+        situacao = FALHA;
+
+    Dinheiro valor;
+    valor.setValor(VALOR_VALIDO);
+    titulo->setValor(valor);
+    if(titulo->getValor().getValor() != VALOR_VALIDO)
+        situacao = FALHA;
+};
+
+void TestePagamento::setUp(){
+    pagamento = new Pagamento();
+    situacao = SUCESSO;
+}
+void TestePagamento::tearDown(){
+    delete pagamento;
+}
+
+void TestePagamento::testarCenarioSucesso(){
+    CodigoPagamento codigo;
+    codigo.setValor(CODIGOPAG_VALIDO);
+    pagamento->setCodigo(codigo);
+    if(pagamento->getCodigo().getValor() != CODIGOPAG_VALIDO)
+        situacao = FALHA;
+
+    Data data;
+    data.setValor(DATA_VALIDA);
+    pagamento->setData(data);
+    if(pagamento->getData().getValor() != DATA_VALIDA)
+        situacao = FALHA;
+
+    Percentual percentual;
+    percentual.setValor(PERCENTUAL_VALIDO);
+    pagamento->setPercentual(percentual);
+    if(pagamento->getPercentual().getValor() != PERCENTUAL_VALIDO)
+        situacao = FALHA;
+
+    Estado estado;
+    estado.setValor(ESTADO_VALIDO);
+    pagamento->setEstado(estado);
+    if(pagamento->getEstado().getValor() != ESTADO_VALIDO)
+        situacao = FALHA;
+};
--- testes_entidades.h
+++ testes_entidades.h
@@ -8,6 +8,53 @@ using namespace std;
 #include "dominios.h"
 #include "entidades.h"
 
+class TesteEntidade {
+    private:                          
+        virtual void setUp()= 0;    
+        virtual void tearDown()= 0;  
+        virtual void testarCenarioSucesso()= 0;
+    protected:
+        string situacao;   
+    public:
+        const string SUCESSO =  "Sucesso!";
+        const string FALHA = "Erro.";
+        string run();
+};
+
+class TesteConta : public TesteEntidade {
+    const string CPF_VALIDO = "343.539.501-00";
+    const string NOME_VALIDO = "Valeria Penna";
+    const string SENHA_VALIDA = "586794";  
+    Conta *conta;                       
+    void setUp();                           
+    void tearDown();                        
+    void testarCenarioSucesso();            
+};
+
+class TesteTitulo : public TesteEntidade {
+    const string CODIGOTIT_VALIDO = "LCI00000000";
+    const string EMISSOR_VALIDO = "Wagner Nunes";
+    const string SETOR_VALIDO = "Energia";
+    const string EMISSAO_VALIDA = "01-11-2001";
+    const string VENCIMENTO_VALIDO = "01-11-2099";
+    const string VALOR_VALIDO = "999999";
+    Titulo *titulo;                       
+    void setUp();                           
+    void tearDown();                        
+    void testarCenarioSucesso();            
+};
+
+class TestePagamento : public TesteEntidade {
+private:
+    const string CODIGOPAG_VALIDO = "87654321";
+    const string DATA_VALIDA = "31-07-2068";
+    const string PERCENTUAL_VALIDO = "69";  
+    const string ESTADO_VALIDO = "Inadimplente";
+    Pagamento *pagamento;                       
+    void setUp();                           
+    void tearDown();                        
+    void testarCenarioSucesso();            
+};
 
 
 #endif // TESTES_H_ENT_INCLUDED
